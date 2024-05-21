80a0acb6addd08bfd485a3aa0358e67f57440cc6
Nicolas Castro <castro.nicolasalain@gmail.com>
2024-05-21 22:43:37 +0200

    chore: adding all of the html and css of starting point

---
+++ src/lib/timelines/__tests__/it-works-.spec.ts
@@ -0,0 +1,8 @@
+import {describe, it} from "vitest"
+
+
+describe("test suite", () => {
+    it("should work", ({expect}) => {
+        expect(true).toBe(true)
+    })
+})
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import User from './pages/User'
+import Footer from './components/Footer'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User/>} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
 )
